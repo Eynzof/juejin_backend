225ea591942197ba934f3f46f9716c6616a41fd5
enzo <Eynzof@gmail.com>
2023-01-30 15:14:12 +0800

    initial commit

---
+++ src/api/menu-tagged/routes/menu-tagged.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * menu-tagged router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::menu-tagged.menu-tagged');
